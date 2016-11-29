d871cff152aeda4ca192e6a81a2e10357eb0f0ae
peter0749 <peter0749@users.noreply.github.com>
2016-11-29 15:43:28 +0800

    Add files via upload

---
+++ hw5/hw5_test.s
@@ -0,0 +1,43 @@
+.data
+    .type arr, %object
+arr:
+    .space 40, 0 @10ints
+
+    .type res, %object
+res:
+    .space 40, 0 @10ints
+
+.section .text
+.global main
+.type main,%function
+
+.arrs:
+    .word arr
+    .word res
+
+main:
+    MOV ip, sp
+    STMFD sp!, {fp,ip,lr,pc}
+    SUB fp, ip, #4
+
+    LDR r0, .arrs   @input array
+    ADD r3, r0, #40 @result array
+    MOV r1, #10  @array size
+    MOV r2, r1   @array size
+STORELOOP:
+    LDMIA r0!, {r4-r7}
+    STMIA r3!, {r4-r7}
+    SUB r2, r2, #1
+    CMP r2, #0
+    BNE STORELOOP
+    LDR r0, .arrs
+    ADD r3, r0, #40
+
+    STR r3, [sp, #-4]!
+    STR r1, [sp, #-4]!
+
+    bl NumSort
+
+    LDMEA fp, {fp,sp,pc}
+    nop
+    .end
---
+++ hw5/numsort.s
@@ -0,0 +1,39 @@
+.section .text
+    .global NumSort
+    .type NumSort, %function
+
+NumSort:
+    MOV ip, sp
+    STMFD sp!,{r0-r10, fp, ip, lr, pc}
+    SUB fp, ip, #4
+
+    LDR r5, [ip], #4 @get array size
+    LDR r6, [ip], #4 @get array address
+    /*Num Sort*/
+    ADD r0, r6, #4 @index of outer loop, i=1
+    ADD r7, r6, r5, LSL #2 @index of full array
+PIVOTLOOP:
+    LDR r3, [r0]
+    MOV r1, r0
+    LDR r8, [r1]
+
+    INNERLOOP:
+        LDR r9, [r8, #-4]
+        STR r8, [r9]
+        SUB r1, r1, #4
+        CMP r1, r6
+        BEQ BREAKLOOP
+        LDR r8, [r1]
+        CMP r3, r8
+        BLT BREAKLOOP
+        B INNERLOOP
+    BREAKLOOP:
+    LDR r1, [r3]
+    ADD r0, r0, #4
+    CMP r0, r7
+    BNE PIVOTLOOP
+    /*End Numsort*/
+
+    nop
+    LDMEA fp, {r0-r10, fp, sp, pc}
+    .end

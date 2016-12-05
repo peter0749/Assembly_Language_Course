146f0bf3953c9036b71904397103dcfb88cb264c
Kuang-Yu Jeng <jengku@gmail.com>
2016-12-05 15:31:31 +0800

    .

---
+++ Assignment5/404410030_hw6/call_numsort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#define MAXN 20
+extern int* NumSort(int, int*);
+
+int main(void){
+    int i, a[MAXN], *res;
+    srand(time(NULL));
+    for(i=0; i<MAXN; ++i)a[i]=rand()%MAXN;
+    res = NumSort(MAXN,a);
+    printf("%d",res[0]);
+    for(i=1; i<MAXN; ++i) printf(" %d",res[i]);
+    printf("\n");
+    return 0;
+}
---
+++ Assignment5/404410030_hw6/numsort.s
@@ -0,0 +1,56 @@
+.section .data
+.type result, %object
+.size result, 400
+result:
+    .space 400
+
+.section .text
+    .global NumSort
+    .type NumSort, %function
+.res:
+    .word result
+
+/*Implement: insertion sort*/
+NumSort:
+    MOV ip, sp
+    STMFD sp!,{r4-r10, fp, ip, lr, pc} /*Callee save registers*/
+    SUB fp, ip, #4
+
+    mov r5, r0 @get array size
+    mov r6, r1 @get array address
+    ldr r2, .res
+COPY:
+    LDMIA r1!, {r7-r10}
+    STMIA r2!, {r7-r10}
+    sub r0, r0, #4
+    cmp r0, #0
+    bgt COPY
+    ldr r6, .res
+    /*Num Sort*/
+    ADD r0, r6, #4 @index of outer loop, i=1
+    ADD r7, r6, r5, LSL #2 @index of full array
+PIVOTLOOP:
+    LDR r3, [r0]
+    MOV r1, r0
+    LDR r8, [r1]
+
+    INNERLOOP:
+        LDR r9, [r1, #-4]
+        STR r9, [r1]
+        SUB r1, r1, #4
+        CMP r1, r6
+        BEQ BREAKLOOP
+        LDR r8, [r1]
+        CMP r3, r8
+        BLT BREAKLOOP
+        B INNERLOOP
+    BREAKLOOP:
+    STR r3, [r1]
+    ADD r0, r0, #4
+    CMP r0, r7
+    BNE PIVOTLOOP
+    /*End Numsort*/
+    MOV r0, r6/*Return pointer*/
+    nop
+    LDMEA fp, {r4-r10, fp, sp, pc} /*Callee save register*/
+    .end

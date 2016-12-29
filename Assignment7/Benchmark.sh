d88052a30476d31361292a656c509d12e94dc4c6
peter0749 <peter0749@yahoo.com.tw>
2016-12-29 10:54:01 +0800

    bench

--- Assignment7/Benchmark.sh
+++ Assignment7/Benchmark.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
 
-for i in {1..32}
+rm -f serial.log
+rm -f parallel.log
+
+for i in {1..128}
 do
     echo "case: $i"
     t1=`./tmp1.bin data.txt`
     echo -e "Serial:\n${t1}us"
     t2=`./tmp2.bin data.txt $i`
     echo -e "OMP+SIMD($i threads):\n${t2}us\n"
+    echo "$t1" >> "serial.log"
+    echo "$t2" >> "parallel.log"
 done
 
